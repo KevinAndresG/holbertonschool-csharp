c4bd08cca53b2818241fad1992e565a026fb3129
KevinAndresG <3348@holbertonschool.com>
2022-04-11 18:38:32 -0500

    fixing

--- 0x00-csharp-hello_world/3-writeline/3-writeline.cs
+++ 0x00-csharp-hello_world/3-writeline/3-writeline.cs
@@ -1,1 +1,9 @@
-﻿Console.WriteLine("\"Programming is like building a multilingual puzzle\n");
+﻿using System;
+
+class Hello
+{
+	static void Main()
+	{
+		Console.WriteLine("\"Programming is like building a multilingual puzzle\n");
+	}
+}
