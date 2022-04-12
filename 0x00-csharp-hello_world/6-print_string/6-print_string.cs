8dd346cfaaad69cb3c039c881dec03503162bdde
KevinAndresG <3348@holbertonschool.com>
2022-04-12 17:52:45 -0500

    printing x times a string without loops or conditions

---
+++ 0x00-csharp-hello_world/6-print_string/6-print_string.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		string str = "Holberton School";
+		string less = str.Substring(0, 9);
+		string threeTimes = string.Concat(Enumerable.Repeat(str, 3));
+
+		Console.WriteLine(threeTimes);
+		Console.WriteLine(less);
+	}
+}
