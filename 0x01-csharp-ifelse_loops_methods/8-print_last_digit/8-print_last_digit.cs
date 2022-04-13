5b8871964561d6cf652387470bc5711b2eafb9e4
KevinAndresG <3348@holbertonschool.com>
2022-04-13 16:19:17 -0500

    print all last digits

---
+++ 0x01-csharp-ifelse_loops_methods/8-print_last_digit/8-print_last_digit.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Number
+{
+	public static int PrintLastDigit(int number)
+	{
+		int num = number % 10;
+
+		if (num < 0)
+		{
+			int numpos = num * -1;
+			Console.Write(numpos);
+			return (numpos);
+		}
+		else
+		{
+			Console.Write(num);
+			return (num);
+		}
+
+	}
+}
