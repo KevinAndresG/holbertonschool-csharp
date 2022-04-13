e5b17fb92ae4a21ceb594c58df54d0acdb83cd70
KevinAndresG <3348@holbertonschool.com>
2022-04-12 19:04:53 -0500

    printing decimal and hexadeciamls numbers to 0 to 98

---
+++ 0x01-csharp-ifelse_loops_methods/4-print_hexa/4-print_hexa.cs
@@ -0,0 +1,14 @@
+﻿using System;
+class Program
+{
+	static void Main(string[] args)
+	{
+		for (int i = 0; i <= 98; i++)
+		{
+			string decimalNumber = i.ToString();
+			int number = int.Parse(decimalNumber);
+			string hex = number.ToString("x");
+			Console.Write($"{i} = 0x{hex}\r\n");
+		}
+	}
+}
