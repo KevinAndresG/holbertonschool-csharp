651ca7abbdaad00299a7b578b874250e5811dc2f
KevinAndresG <3348@holbertonschool.com>
2022-04-12 23:00:38 -0500

    print digits not repeated

---
+++ 0x01-csharp-ifelse_loops_methods/6-print_comb2/6-print_comb2.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		for (int i = 0; i < 9; i++)
+		{
+			for (int j = i; j <= 9; j++)
+			{
+				if (i == 8 && j == 9)
+				{
+					Console.Write($"{i}{j}\n");
+				}
+				else if (i != j)
+				{
+					Console.Write($"{i}{j}, ");
+				}
+			}
+		}
+	}
+}
