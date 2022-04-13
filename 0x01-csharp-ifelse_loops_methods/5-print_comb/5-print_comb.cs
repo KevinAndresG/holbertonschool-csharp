9030ec39fa0b57971b36b6df3ade529c68a37bc6
KevinAndresG <3348@holbertonschool.com>
2022-04-12 22:46:09 -0500

    printing two digits 0 to 99

---
+++ 0x01-csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		for (int i = 0; i <= 99; i++)
+		{
+			if (i != 99)
+			{
+				Console.Write($"{i:D2}, ");
+			}
+			else
+			{
+				Console.Write($"{i:D2}\n");
+			}
+		}
+	}
+}
