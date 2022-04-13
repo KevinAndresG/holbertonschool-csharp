f5ea12419f6730c4d3e7075bb01c7425f41a7ce8
KevinAndresG <3348@holbertonschool.com>
2022-04-12 19:09:23 -0500

    fixing

--- 0x01-csharp-ifelse_loops_methods/4-print_hexa/4-print_hexa.cs
+++ 0x01-csharp-ifelse_loops_methods/4-print_hexa/4-print_hexa.cs
@@ -5,9 +5,7 @@ class Program
 	{
 		for (int i = 0; i <= 98; i++)
 		{
-			string decimalNumber = i.ToString();
-			int number = int.Parse(decimalNumber);
-			string hex = number.ToString("x");
+			string hex = i.ToString("x");
 			Console.Write($"{i} = 0x{hex}\r\n");
 		}
 	}
