ca92954a6ec2a02d2e550f0e80dbddde04e18b24
KevinAndresG <3348@holbertonschool.com>
2022-04-14 13:36:36 -0500

    add unique integers

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/0-unique_add/0-unique_add.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+	public static int Sum(List<int> myList)
+	{
+		int added = 0;
+		foreach (var num in myList.Distinct())
+		{
+			added += num;
+		}
+		return (added);
+	}
+}
