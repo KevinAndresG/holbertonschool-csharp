b80508b9fff78a5aeb3ee6d39404967471a50e8a
KevinAndresG <3348@holbertonschool.com>
2022-04-13 22:28:55 -0500

    print a list

---
+++ 0x02-csharp-arrays_lists_dictionaries/4-print_list/4-print_list.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class List
+{
+	public static List<int> CreatePrint(int size)
+	{
+		if (size < 0)
+		{
+			Console.WriteLine("Size cannot be negative");
+			return (null);
+		}
+		List<int> lista = new List<int>();
+		int i;
+		for (i = 0; i < size; i++)
+		{
+			lista.Add(i);
+			Console.Write($"{lista[i]}");
+			if (i != size - 1)
+			{
+				Console.Write(' ');
+			}
+		}
+		Console.WriteLine();
+		return (lista);
+	}
+}
