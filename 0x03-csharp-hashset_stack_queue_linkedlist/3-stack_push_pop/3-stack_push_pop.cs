771700c45db7f7e0461e2b372e95f77b222e2aeb
KevinAndresG <3348@holbertonschool.com>
2022-04-14 16:10:07 -0500

    replace in a stack

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+class MyStack
+{
+	public static Stack<string> Info(Stack<string> aStack, string newItem, string search)
+	{
+		if (aStack.Count() == 0)
+		{
+			Console.WriteLine("Stack is empty");
+		}
+		int stackSize = aStack.Count();
+		Stack<string> newStack = new Stack<string>();
+
+		Console.WriteLine($"Number of items: {stackSize}");
+		Console.WriteLine($"Top item: {aStack.Peek()}");
+		if (aStack.Contains(search))
+		{
+			string[] i = new string[stackSize];
+			int j = 0;
+
+			Console.WriteLine($"Stack contains {search}: True");
+			while (aStack.Count() > 0)
+			{
+				string words = aStack.Pop();
+
+				i[j] = words;
+				j++;
+			}
+			while (i[j - 1] != search)
+			{
+				aStack.Push(i[j - 1]);
+				j--;
+			}
+			aStack.Push(newItem);
+		}
+		else
+		{
+			Console.WriteLine($"Stack contains {search}: False");
+		}
+		return (aStack);
+	}
+}
