54c92c8731337a3730ae4a242af5104fcd1b6c65
KevinAndresG <3348@holbertonschool.com>
2022-04-14 08:49:43 -0500

    sort the dictionary by key

---
+++ 0x02-csharp-arrays_lists_dictionaries/12-print_sorted_dictionary/12-print_sorted_dictionary.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Dictionary
+{
+	public static void PrintSorted(Dictionary<string, string> myDict)
+	{
+		foreach (KeyValuePair<string, string> newDict in myDict.OrderBy(i => i.Key))
+		{
+			Console.WriteLine($"{newDict.Key}: {newDict.Value}");
+		}
+	}
+}
