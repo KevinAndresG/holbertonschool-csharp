81ab9bc632a716a1214d5fb6b1749c629a635808
KevinAndresG <3348@holbertonschool.com>
2022-04-14 00:13:50 -0500

    fixing

--- 0x00-csharp-hello_world/6-print_string/6-print_string.cs
+++ 0x00-csharp-hello_world/6-print_string/6-print_string.cs
@@ -5,9 +5,9 @@ class Program
 	static void Main(string[] args)
 	{
 		string str = "Holberton School";
-		string threeTimes = String.Concat(Enumerable.Repeat(str, 3));
+		// string threeTimes = String.Concat(Enumerable.Repeat(str, 3));
 		string less = str.Substring(0, 9);
-		Console.WriteLine($"{threeTimes}");
+		Console.WriteLine("{0}{0}{0}", str);
 		Console.Write($"{less}");
 	}
 }
