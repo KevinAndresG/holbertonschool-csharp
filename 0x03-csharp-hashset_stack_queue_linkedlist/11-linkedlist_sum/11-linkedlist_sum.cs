2a8b9b488e573e1ee3c571c3ea21a6506559eadf
KevinAndresG <3348@holbertonschool.com>
2022-04-15 11:10:03 -0500

    add all elements in the llist

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/11-linkedlist_sum/11-linkedlist_sum.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+	public static int Sum(LinkedList<int> myLList)
+	{
+		// int suma = 0;
+		// foreach (int i in myLList)
+		// {
+		// suma += i;
+		// }
+		// return suma;
+		int suma = 0;
+		LinkedListNode<int> newNode = myLList.First;
+		while (newNode != null)
+		{
+			suma += newNode.Value;
+			newNode = newNode.Next;
+		}
+		return (suma);
+	}
+}
