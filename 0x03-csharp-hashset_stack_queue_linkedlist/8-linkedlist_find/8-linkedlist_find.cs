2f0a30552709204702505ecd340630943d310056
KevinAndresG <3348@holbertonschool.com>
2022-04-15 10:24:09 -0500

    find index by value

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/8-linkedlist_find/8-linkedlist_find.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+	public static int FindNode(LinkedList<int> myLList, int value)
+	{
+		int counter = 0;
+		if (myLList.Contains(value))
+		{
+			foreach (int j in myLList)
+			{
+				if (j != value)
+				{
+					counter++;
+				}
+				else
+				{
+					return (counter);
+				}
+
+			}
+		}
+		return (-1);
+	}
+}
