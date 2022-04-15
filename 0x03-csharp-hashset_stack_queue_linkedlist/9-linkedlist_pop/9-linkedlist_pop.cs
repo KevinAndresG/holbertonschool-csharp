1f8ad853b1fbe3f55ce48fe646078e71aba8491e
KevinAndresG <3348@holbertonschool.com>
2022-04-15 10:46:13 -0500

    delete first node and return his value

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/9-linkedlist_pop/9-linkedlist_pop.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+	public static int Pop(LinkedList<int> myLList)
+	{
+		if (myLList.Count == 0)
+		{
+			return (0);
+		}
+		int j = 0;
+		foreach (int i in myLList)
+		{
+			j = i;
+			myLList.RemoveFirst();
+			break;
+		}
+		return (j);
+		// Another form:
+		// LinkedListNode<int> newNode = myLList.First;
+		// myLList.RemoveFirst();
+		// return (newNode.Value);
+	}
+}
