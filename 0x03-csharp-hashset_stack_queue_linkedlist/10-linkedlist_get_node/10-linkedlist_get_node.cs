f831d17db81bc25913ff37df4ed5044450b6f70a
KevinAndresG <3348@holbertonschool.com>
2022-04-15 10:58:40 -0500

    find value by index

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/10-linkedlist_get_node/10-linkedlist_get_node.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+class LList
+{
+	public static int GetNode(LinkedList<int> myLList, int n)
+	{
+		int i = 0;
+		if (n < myLList.Count || n > 0)
+		{
+			foreach (int j in myLList)
+			{
+				if (i == n)
+				{
+					return (j);
+				}
+				i++;
+			}
+		}
+		return (0);
+	}
+}
