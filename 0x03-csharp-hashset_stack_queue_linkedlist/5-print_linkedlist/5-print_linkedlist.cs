eaaf65df6cecc02fc40e25274435c18c59c80413
KevinAndresG <3348@holbertonschool.com>
2022-04-14 21:41:43 -0500

    fill and print linked list

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/5-print_linkedlist/5-print_linkedlist.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+	public static LinkedList<int> CreatePrint(int size)
+	{
+		LinkedList<int> newlist = new LinkedList<int>();
+		int i;
+
+		for (i = 0; i < size; i++)
+		{
+			newlist.AddLast(i);
+		}
+		foreach (int j in newlist)
+		{
+			Console.WriteLine(j);
+		}
+		return (newlist);
+	}
+}
