19779d860188d68d1265a4598c9e0042e2704ad9
KevinAndresG <3348@holbertonschool.com>
2022-04-15 10:12:15 -0500

    add node start

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/7-linkedlist_add/7-linkedlist_add.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+class LList
+{
+	public static LinkedListNode<int> Add(LinkedList<int> myLList, int n)
+	{
+		myLList.AddFirst(n);
+		LinkedListNode<int> newNode = myLList.First;
+		return (newNode);
+	}
+}
