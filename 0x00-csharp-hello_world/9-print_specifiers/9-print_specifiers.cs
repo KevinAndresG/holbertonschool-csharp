25fc35cdec872be37242ad0a2269290cc32696cb
KevinAndresG <3348@holbertonschool.com>
2022-04-17 15:18:48 -0500

    fixing

--- 0x00-csharp-hello_world/9-print_specifiers/9-print_specifiers.cs
+++ 0x00-csharp-hello_world/9-print_specifiers/9-print_specifiers.cs
@@ -1,11 +1,12 @@
 ﻿using System;
-
+using System.Globalization;
 class Program
 {
 	static void Main(string[] args)
 	{
+		CultureInfo target = CultureInfo.CreateSpecificCulture("en-US.utf-8");
 		double percent = .7553;
-		double currency = 98765.4321;
-		Console.WriteLine("Percent: {0:P}\nCurrency: {1:C}", percent, currency);
+		double currency = -98765.4321;
+		Console.WriteLine($"Percent: {String.Format(target, "{0:P}", percent)}\nCurrency: {String.Format(target, "{0:C}", currency)}");
 	}
 }
