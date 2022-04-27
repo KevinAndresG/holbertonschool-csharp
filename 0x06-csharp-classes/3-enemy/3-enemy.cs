baddccf3fbab4b4b754639ac5b266fbf709d8e08
KevinAndresG <3348@holbertonschool.com>
2022-04-26 22:47:47 -0500

    new method to return the health

---
+++ 0x06-csharp-classes/3-enemy/3-enemy.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Enemies
+{
+	///<summary>Provides a zombie definition.</summary>
+	public class Zombie
+	{
+		///<summary>Field to define the health.</summary>
+		public int health;
+		///<summary>set zombie's health in 0.</summary>
+		public Zombie()
+		{
+			health = 0;
+		}
+		///<summary>Check if valie is greater than 0.</summary>
+		public Zombie(int value)
+		{
+			if (value < 0)
+			{
+				throw new ArgumentException("Health must be greater than or equal to 0");
+			}
+			this.health = value;
+		}
+		///<Returns>the value of health of the Zombie object.</Returns>
+		public int GetHealth()
+		{
+			return health;
+		}
+	}
+}
