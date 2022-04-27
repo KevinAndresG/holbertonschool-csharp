8e25c93dcd0c9b5a91bef45c6d1bd997342ca7b8
KevinAndresG <3348@holbertonschool.com>
2022-04-26 22:27:58 -0500

    creating new constructor and validate the value

---
+++ 0x06-csharp-classes/2-enemy/2-enemy.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Enemies
+{
+	///<summary>Provides a zombie definition.</summary>
+	public class Zombie
+	{
+		///<summary>Field to define the health.</summary>
+		public int health;
+		///<summary>set zombie's health in 0.</summary>
+		public Zombie()
+		{
+			this.health = 0;
+		}
+		///<summary>Check if valie is greater than 0.</summary>
+		public Zombie(int value)
+		{
+			if (value < 0)
+			{
+				throw new ArgumentException("Health must be greater than or equal to 0");
+			}
+			this.health = value;
+		}
+	}
+}
