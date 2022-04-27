952dbc120965396b51f8f3d736bf2884d04ca6ad
KevinAndresG <3348@holbertonschool.com>
2022-04-26 22:16:57 -0500

    create the field and set it in 0

---
+++ 0x06-csharp-classes/1-enemy/1-enemy.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Enemies
+{
+	///<summary>Provides a zombie definition.</summary>
+	public class Zombie
+	{
+		///<summary>Field to define the health.</summary>
+		public int health;
+		///<summary>set zombie's health in 0.</summary>
+		public Zombie()
+		{
+			this.health = 0;
+		}
+	}
+}
---
+++ 0x06-csharp-classes/CreateFiles.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+dotnet new console -n 1-enemy
+cd 1-enemy
+touch 1-main.cs
