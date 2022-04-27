47f9040c11b02ba2f489434f87c2118c3532613c
KevinAndresG <3348@holbertonschool.com>
2022-04-26 23:01:57 -0500

    Return the format object attributes

---
+++ 0x06-csharp-classes/5-enemy/5-enemy.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Enemies
+{
+	///<summary>Provides a zombie definition.</summary>
+	public class Zombie
+	{
+		///<summary>Field to define the health.</summary>
+		public int health;
+		///<summary>Field to define the name.</summary>
+		private string name = "(No name)";
+		///<summary>get and set the values.</summary>
+		public string Name
+		{
+			get { return name; }
+			set { name = value; }
+		}
+		///<summary>set zombie's health in 0.</summary>
+		public Zombie()
+		{
+			this.health = 0;
+		}
+		///<summary>Check if valie is greater than 0.</summary>
+		public Zombie(int value)
+		{
+			if (value < 0)
+			{
+				throw new ArgumentException("Health must be greater than or equal to 0");
+			}
+			this.health = value;
+		}
+		///<Returns>the value of health of the Zombie object.</Returns>
+		public int GetHealth()
+		{
+			return health;
+		}
+		///<Returns>the format object attributes</Returns>
+		public override string ToString()
+		{
+			return ($"Zombie Name: {this.name} / Total Health: {this.health}");
+		}
+	}
+}
