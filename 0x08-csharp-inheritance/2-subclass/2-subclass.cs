545ae765a8b8a0abda263ec561d4ed9d1d3a2eb7
KevinAndresG <3348@holbertonschool.com>
2022-05-02 23:38:18 -0500

    only if is a subclass of

---
+++ 0x08-csharp-inheritance/2-subclass/2-subclass.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+///<sumary>Object classes</sumary>
+public class Obj
+{
+	///<Returns>Return true if is an instace of specific class</Returns>
+	public static bool IsOnlyASubclass(Type derivedType, Type baseType)
+	{
+		if (derivedType.IsSubclassOf(baseType))
+		{
+			return (true);
+		}
+		else
+		{
+			return (false);
+		}
+	}
+}
