bee8c1e399c92c32c00613993470b9b9c5356743
KevinAndresG <3348@holbertonschool.com>
2022-05-03 14:02:43 -0500

    rint all methods and properties of a class

---
+++ 0x08-csharp-inheritance/3-type_get/3-type_get.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+///<sumary>Object classes</sumary>
+class Obj
+{
+	///<sumary>print all methods and properties of an obj</sumary>
+	public static void Print(object myObj)
+	{
+		PropertyInfo[] myPropertyInfo;
+		myPropertyInfo = myObj.GetType().GetProperties();
+
+		MethodInfo[] myMethodInfo;
+		myMethodInfo = myObj.GetType().GetMethods();
+
+		Console.WriteLine("{0} Properties:", myObj.GetType());
+		for (int i = 0; i < myPropertyInfo.Length; i++)
+		{
+			Console.WriteLine(myPropertyInfo[i]);
+		}
+
+		Console.WriteLine("{0} Methods:", myObj.GetType());
+		for (int j = 0; j < myMethodInfo.Length; j++)
+		{
+			Console.WriteLine(myMethodInfo[j]);
+		}
+	}
+}
