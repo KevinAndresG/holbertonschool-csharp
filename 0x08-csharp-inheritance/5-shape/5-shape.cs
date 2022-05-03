d40b931250de589acdfa30578c7583966fe288a5
KevinAndresG <3348@holbertonschool.com>
2022-05-03 15:52:42 -0500

    get and set the properties

--- 0x08-csharp-inheritance/5-shape/5-shape.cs
+++ 0x08-csharp-inheritance/5-shape/5-shape.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 
+///<sumary>some shapes</sumary>
 class Shape
 {
+	///<sumary>Empty for now</sumary>
 	public virtual int Area()
 	{
 		throw new NotImplementedException("Area() is not implemented");
---
+++ 0x08-csharp-inheritance/6-shape/6-shape.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+///<sumary>some shapes</sumary>
+class Shape
+{
+	public virtual int Area()
+	{
+		throw new NotImplementedException("Area() is not implemented");
+	}
+}
+class Rectangle : Shape
+{
+	private int width;
+	private int height;
+	public int Width
+	{
+		get { return width; }
+		set
+		{
+			if (value < 0)
+			{
+				throw new ArgumentException("Width must be greater than or equal to 0");
+			}
+			else
+			{
+				width = value;
+			}
+		}
+	}
+	public int Height
+	{
+		get { return height; }
+		set
+		{
+			if (value < 0)
+			{
+				throw new ArgumentException("Height must be greater than or equal to 0");
+			}
+			else
+			{
+				height= value;
+			}
+		}
+	}
+}
