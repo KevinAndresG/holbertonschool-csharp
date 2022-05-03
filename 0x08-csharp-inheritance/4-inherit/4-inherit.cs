1c6a7325d6d5812f753624c9e27ee7bbdf3ca7d5
KevinAndresG <3348@holbertonschool.com>
2022-05-03 14:15:30 -0500

    empty base class and derived class

---
+++ 0x08-csharp-inheritance/4-inherit/4-inherit.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+///<sumary>Empty BASE class for now</sumary>
+class Animal
+{
+	//Empty class
+}
+///<sumary>Empty BASE class for now</sumary>
+class Dog : Animal
+{
+	//Empty class
+}
