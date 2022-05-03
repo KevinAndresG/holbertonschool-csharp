2b99ae9b4b994843d3233c62767079e7f8f43210
KevinAndresG <3348@holbertonschool.com>
2022-05-02 23:25:38 -0500

    returning tru if is an instance of

---
+++ 0x08-csharp-inheritance/1-instance/1-instance.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+///<sumary>Object classes</sumary>
+public class Obj
+{
+	///<Returns>Return true if is an instace of </Returns>
+	public static bool IsInstanceOfArray(object obj)
+	{
+		if (typeof(Array).IsInstanceOfType(obj))
+		{
+			return (true);
+		}
+		else
+		{
+			return (false);
+		}
+	}
+}
