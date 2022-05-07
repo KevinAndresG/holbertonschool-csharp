d59f2fe857d67c6763a33d1c4f9cc0584fbc0ef2
KevinAndresG <3348@holbertonschool.com>
2022-05-07 15:25:52 -0500

    just a phrase

---
+++ 0x00-csharp-hello_world/11-concat_edges/11-concat_edges.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		string str = "C# (pronounced \"See Sharp\") is a simple, modern, object-oriented, and type-safe programming language. C# has its roots in the C family of languages and will be immediately familiar to C, C++, Java, and JavaScript programmers.";
+		str = str.Substring(49, str.Length -210) + str.Substring(121, str.Length -224) + str.Substring(80, str.Length -213) + str.Substring(119, str.Length -222) + str.Substring(102, str.Length -223);
+		Console.WriteLine(str);
+    }
+}
