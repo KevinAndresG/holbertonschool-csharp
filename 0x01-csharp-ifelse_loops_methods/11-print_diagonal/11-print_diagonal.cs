a86cf6a0c420672df87ed6bb881dabb10f61f467
KevinAndresG <3348@holbertonschool.com>
2022-05-07 15:38:28 -0500

    fizzbuz and diagonal

---
+++ 0x01-csharp-ifelse_loops_methods/11-print_diagonal/11-print_diagonal.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Line {
+    public static void PrintDiagonal(int length) {
+        if (length == 0 || length < 0) {
+            Console.WriteLine();
+            return;
+        }
+        for (int i = 1; i <= length; i++) {
+            Console.WriteLine("\\");
+            if (i != length) {
+                for (int j = 1; j <= i; j++) {
+                    Console.Write(" ");
+                }
+            } else {
+                Console.WriteLine();
+            }
+        }
+    }
+}
