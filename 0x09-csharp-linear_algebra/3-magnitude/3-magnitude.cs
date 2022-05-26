90f11df0d4130e5509fbefa2be20d07507b0d291
KevinAndresG <kevingar1414@gmail.com>
2022-05-26 14:14:56 -0500

    fix files and return size of a 2D or 3D Vector

---
+++ 0x09-csharp-linear_algebra/3-magnitude/3-magnitude.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+///<summary>Provides the size of a Vector</summary>
+class VectorMtah
+{
+    ///<Returns>the size of the vector 2D or 3D</Returns>
+    public static double Magnitude(double[] vector)
+    {
+        if (vector.Length == 2)
+        {
+            // Logic for 2D Vector
+            return Math.Round(Math.Sqrt(Math.Pow(vector[0], 2) + Math.Pow(vector[1], 2)));
+        }
+        if (vector.Length == 3)
+        {
+            // Logic for 3D Vector
+            return Math.Round(Math.Sqrt(Math.Pow(vector[0], 2) + Math.Pow(vector[1], 2) + Math.Pow(vector[2], 2)));
+        }
+        return (-1);
+    }
+}
