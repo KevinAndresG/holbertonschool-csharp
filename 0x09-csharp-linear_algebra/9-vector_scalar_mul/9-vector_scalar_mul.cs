65e113b1bae8d95abba97cf85e1dbafae6534582
KevinAndresG <kevingar1414@gmail.com>
2022-05-26 16:30:51 -0500

    multiply two vectors

---
+++ 0x09-csharp-linear_algebra/9-vector_scalar_mul/9-vector_scalar_mul.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+///<summary>Provides the multiplying of two Vectors</summary>
+class VectorMath
+{
+    ///<Returns>two vectors multiplyied</Returns>
+    public static double[] Multiply(double[] vector, double scalar)
+    {
+        double[] errorVector = new double[1];
+        if (vector.Length == 2)
+        {
+            //Logic for 2D Vectors
+            double[] twoDVector = new double[2];
+            for (int i = 0; i < 2; i++)
+            {
+                twoDVector[i] = vector[i] * scalar;
+            }
+            return (twoDVector);
+        }
+        if (vector.Length == 3)
+        {
+            //Logic for 3D Vectors
+            double[] threeDVector = new double[3];
+            for (int i = 0; i < 3; i++)
+            {
+                threeDVector[i] = vector[i] * scalar;
+            }
+            return (threeDVector);
+        }
+        else
+            errorVector[0] = -1;
+        return (errorVector);
+    }
+}
