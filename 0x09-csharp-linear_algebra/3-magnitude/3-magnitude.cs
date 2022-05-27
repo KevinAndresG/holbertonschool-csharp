c5d95db1b9448ba16dbd7454272a644e0a31ff17
KevinAndresG <kevingar1414@gmail.com>
2022-05-26 23:12:12 -0500

    modifying

--- 0x09-csharp-linear_algebra/3-magnitude/3-magnitude.cs
+++ 0x09-csharp-linear_algebra/3-magnitude/3-magnitude.cs
@@ -7,15 +7,18 @@ class VectorMath
     ///<Returns>the size of the vector 2D or 3D</Returns>
     public static double Magnitude(double[] vector)
     {
+        double newVector;
         if (vector.Length == 2)
         {
             // Logic for 2D Vector
-            return Math.Floor(Math.Sqrt(Math.Pow(vector[0], 2) + Math.Pow(vector[1], 2)));
+            newVector = Math.Round(Math.Sqrt(Math.Pow(vector[0], 2) + Math.Pow(vector[1], 2)));
+            return newVector;
         }
         else if (vector.Length == 3)
         {
             // Logic for 3D Vector
-            return Math.Floor(Math.Sqrt(Math.Pow(vector[0], 2) + Math.Pow(vector[1], 2) + Math.Pow(vector[2], 2)));
+            newVector = Math.Round(Math.Sqrt(Math.Pow(vector[0], 2) + Math.Pow(vector[1], 2) + Math.Pow(vector[2], 2)));
+            return newVector;
         }
         return (-1);
     }
